996a9a5b0a9002a7a11649c8dbc949d5e97add25
Shivanagoud Annigeri <sgouda1999@gmail.com>
2023-05-04 12:49:52 +0530

    first commit

---
+++ {{cookiecutter.projectname}}/src/main/java/com/{{cookiecutter.appName}}.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class {{cookiecutter.entityName}}CrudSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run({{cookiecutter.entity_name}}CrudSpringBootApplication.class, args);
+	}
+
+}
