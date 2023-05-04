87218d53b0f18ed651c63bed3592b73e6638c958
Shivanagoud Annigeri <sgouda1999@gmail.com>
2023-05-04 15:04:26 +0530

    4th commit

--- {{cookiecutter.projectname}}/src/main/java/com/{{cookiecutter.companyName}}/{{cookiecutter.appName}}.java
+++ {{cookiecutter.projectname}}/src/main/java/com/{{cookiecutter.companyName}}/{{cookiecutter.appName}}.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class {{cookiecutter.entityName}}CrudSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run({{cookiecutter.entity_name}}CrudSpringBootApplication.class, args);
+		SpringApplication.run({{cookiecutter.entityName}}CrudSpringBootApplication.class, args);
 	}
 
 }
